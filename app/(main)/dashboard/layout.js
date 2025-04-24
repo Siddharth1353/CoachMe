9184636e688170f26cec63bd1564ddb3664a1722
Siddharth1353 <siddharthrajpurohit235@gmail.com>
2025-04-25 02:11:28 +0530

    updated

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,20 @@
+import React, { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+const Layout = ({ children }) => {
+  return (
+    <div className="px-5">
+      <div className="flex itesm-center justify-between mb-5">
+        <h1 className="text-6xl font-bold bg-gradient-to-b from-gray-400 via-gray-200 to-gray-600 font-extrabold tracking-tighter text-transparent bg-clip-text pb-2 pr-2  md:text-6xl ">
+          IndustryInsight
+        </h1>
+      </div>
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
+};
+
+export default Layout;
